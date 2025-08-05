0d397e05807f249e0a612a51b21acb8b97324efe
nazimakasymova <nozimakhon.k1@gmail.com>
2025-08-05 01:41:24 +0000

    ADDED CREDS

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider aws {
+    region = "us-east-2"
+}
+
+resource aws_instance web {
+    ami           = "ami-08ca1d1e465fbfe0c"
+    instance_type = "t3.micro"
+}
+
+React
+
+Reply
+
+
+
+
+
+
+
+
+
+
+
+
+
+Message may-group-2025
+
+
+
+
